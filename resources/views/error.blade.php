0b33e8867a42304a49acec878c4bb351747b0f22
deepakkumar5686 <deepakkr8156@gmail.com>
2024-03-04 16:13:02 +0530

    add external css link

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -6,37 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Access Denied</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            color: #333;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-            text-align: center;
-        }
+    <link  rel="stylesheet" href="{{asset('css/error.css')}}">
 
-        .container {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        h1 {
-            color: #d9534f;
-        }
-
-        p {
-            margin-top: 20px;
-            font-size: 18px;
-            line-height: 1.6;
-        }
-    </style>
 </head>
 
 <body>
